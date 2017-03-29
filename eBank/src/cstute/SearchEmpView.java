50569e941ae067ba87acf31d59129c8157fe2c5f
Achala007 <achalayasas007@gmail.com>
2017-03-29 23:18:06 +0530

    j button

--- eBank/src/cstute/SearchEmpView.java
+++ eBank/src/cstute/SearchEmpView.java
@@ -280,11 +280,7 @@ public class SearchEmpView extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        CustomerUp.startCust();
-        dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+  //GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
