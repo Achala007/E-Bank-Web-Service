92bd071fb2145e964aacc6016faef1005d314d0b
Achala007 <achalayasas007@gmail.com>
2017-04-01 23:21:17 +0530

    menue2

--- eBank/src/cstute/Selections.java
+++ eBank/src/cstute/Selections.java
@@ -53,11 +53,6 @@ public class Selections extends javax.swing.JFrame {
             }
         });
 
-        custListBtn.setText("Customer List");
-        custListBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                custListBtnActionPerformed(evt);
-            }
         });
 
         mngCustBtn.setText("Manage Customers");
