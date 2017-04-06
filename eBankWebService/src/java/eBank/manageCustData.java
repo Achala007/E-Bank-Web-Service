683eb017ebc7cb4aa5d32b723ce5cfa2902ce9af
Achala007 <achalayasas007@gmail.com>
2017-04-06 10:31:24 +0530

    conn close

--- eBankWebService/src/java/eBank/manageCustData.java
+++ eBankWebService/src/java/eBank/manageCustData.java
@@ -67,6 +67,7 @@ public class manageCustData {
                 if (affectedRows > 0) {
                     return "Successfully added the customer to the database.";
                 }
+            con.close();
 
         } catch (SQLException e) {
             if (e.getErrorCode() == 1062) {
