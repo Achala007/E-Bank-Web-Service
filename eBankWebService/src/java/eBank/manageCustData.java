f7d250280f802bbe31846a99570f9786ee5d07d7
Achala007 <achalayasas007@gmail.com>
2017-04-08 10:34:47 +0530

    database3

--- eBankWebService/src/java/eBank/manageCustData.java
+++ eBankWebService/src/java/eBank/manageCustData.java
@@ -51,7 +51,8 @@ public class manageCustData {
             preparedStmt.setInt(4, mobile);
             preparedStmt.setString(5, email);
             preparedStmt.setString(6, acntType);
-            preparedStmt.setInt(7, acntNuode);
+            preparedStmt.setInt(7, acntNumber);
+            preparedStmt.setString(8, sortCode);
             preparedStmt.setDouble(9, balance);
             preparedStmt.setString(10, card);
             // execute the preparedstatement
